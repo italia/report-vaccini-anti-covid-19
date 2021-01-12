b497fd467be1e41a00a596736a6e669c425669fc
GRUPPOSI\utitola <utitola@invitalia.it>
2021-01-12 16:48:15 +0100

    footer redirect link

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -13,22 +13,35 @@ export const HeaderBar = () => {
       </Header> */}
       <div className="row m-0" style={{ backgroundColor: '#0059b3' }}>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2">
-             <a href="http://www.governo.it" target="_blank" rel="noreferrer">
-              <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <a href="http://www.governo.it" className="text-decoration-none" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          </a>
+          <p className="pt-sm-0">
+            <a href="http://www.governo.it" target="_blank" className="text-decoration-none" rel="noreferrer">
+              Presidenza del Consiglio dei Ministri
             </a>
-            <p className="pt-sm-0">Presidenza del Consiglio dei Ministri</p>
+          </p>
         </div>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pr-sm-0 pt-2 pb-2">
-            <a href="http://www.governo.it" target="_blank" rel="noreferrer">
-              <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <a className="text-decoration-none" href="http://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          </a>
+          <p className="pt-sm-0">
+            <a className="text-decoration-none" href="http://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
+              Commissario Straordinario Covid-19
             </a>
-            <p className="pt-sm-0">Commissario Straordinario Covid-19</p>
+          </p>
         </div>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2 pr-sm-3">
-            <a href="http://www.governo.it" target="_blank" rel="noreferrer">
-              <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
-            </a>
-            <p className="pt-sm-0">Ministero della Salute</p>
+          <a href=" http://www.salute.gov.it/portale/home.html" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          </a>
+          <p className="pt-sm-0">
+            <a href="http://www.salute.gov.it/portale/home.html" className="text-decoration-none" target="_blank" rel="noreferrer">
+              Ministero della Salute
+              </a>
+          </p>
+
         </div>
         {/** TITLE + LOGO*/}
         <div className="col-12 d-flex flex-column justify-content-center mainBanner">
