a2e1cc4dfac58083926c92e2adc03a2e909ce558
Francesco <francesco.clementi@mail-bip.com>
2021-01-19 11:56:46 +0100

    Removed unused import on BarChart

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useRef, useState } from "react";
+import { React, useEffect, useRef } from "react";
 import * as d3 from "d3";
 import "../App.css";
 import { maxX } from "../utils";
