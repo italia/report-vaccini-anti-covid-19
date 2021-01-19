10b799590f15b21e3a958d0c4f54f5cdf7fa5aaa
Francesco <francesco.clementi@mail-bip.com>
2021-01-19 10:15:14 +0100

    Cleaning unused code

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import { MapArea } from "./components/MapArea";
 import { MapAreaByCat } from "./components/MapAreaByCat";
 import { MapAreaByDeliveryLocation } from "./components/MapAreaByDeliveryLocation";
 import { StaticBlock } from "./components/StaticBlock";
-import { LocationsTable } from "./components/LocationsTable";
-import { Table } from "./components/Table";
+
 import { Total } from "./components/Total";
 import { loadData } from "./loadData";
 import { Deliveries } from "./containers/deliveries";
 import { Categories } from "./containers/categories";
 import { Supplier } from './components/Supplier';
-import { areaMappingReverse, groupByAge, allTotalGender, hideLoader, simulateClick } from "./utils";
+import { hideLoader, simulateClick } from "./utils";
 import * as _ from 'lodash';
 import "./App.css";
 import { omit } from "lodash";
+import { Locations } from "./containers/locations";
 
 
 function App() {
@@ -68,55 +68,9 @@ function App() {
     });
     setSelectedFilterByAge(_summary);
   }
-  const handleRectClick = (currentRect) => {
-    let currentRectExist = currentRect ? true : false;
-    let selectedExist = selected ? true : false;
-    if (!currentRectExist) {
-      setBarState(summary.categoriesAndAges);
-      setTotalAgeByGender(summary.gender);
-      setSelectedFilterByAge(null);
-      setSelectedAge(null)
-      setSelected(null);
-    }
-    if (currentRectExist && selectedExist) {
-      resetFilter();
-      let currentRectDefault = summary?.categoriesAndAges.filter((e) => e?.fascia_anagrafica === currentRect?.fascia_anagrafica);
-      setTableFilteredVaccini(currentRect);
-      loadRect(currentRectDefault[0])
-    }
-    if (currentRectExist && !selectedExist) {
-      setTableFilteredVaccini(currentRect);
-      loadRect(currentRect);
-    }
-  }
-  const handleHRectClick = (currentRect) => {
-    if (currentRect) {
-      setSelectedCodeCategory(currentRect?.code)
-    } else {
-      setSelectedCodeCategory(null)
-    }
-  }
-
 
   const handleCountryClickLocations = (countryIndex) => {
     setSelectedLocation(countryIndex);
-  };
-
-  const handleCountryClickCategories = (countryIndex) => {
-    setSelectedCodeCategory(null)
-    const area = summary.deliverySummary[countryIndex]?.area;
-    const areaCode = areaMappingReverse[area];
-    const data = summary.categoriesByRegions[areaCode];
-
-    let _selected = summary.deliverySummary[countryIndex];
-    setSelectedLocationCategoryMap(_selected);
-
-    setTotalByCategory(
-      countryIndex ? _selected.dosi_somministrate : summary.tot
-    )
-    setSelectedCategory(
-      countryIndex ? data?.slice() || [] : summary.categories
-    );
   };
 
   useEffect(() => {
@@ -213,88 +167,9 @@ function App() {
           data={summary}
         />
         <Supplier data={summary}></Supplier>
-
-        <div className="row ">
-          <div
-            className="col-12 d-flex justify-content-center align-items-center p-5 bg-title-plot">
-            <img src="logo.png" width="86" height="86" alt="Logo" className="img-fluid" style={{ zIndex: 10 }} />
-            <h3 className="text-center">Punti di somministrazione per regione</h3>
-          </div>
-          <div className="col-12 col-md-12 h-100 p-0">
-            <div className="mb-5  d-lg-none " style={{
-              position: 'relative',
-              background: '#013366',
-
-            }}>
-              <div className="text-white w-100">
-                <div className="w-100  h-100 d-flex justify-content-start pt-5 pl-4">
-                  <h5>Punti di somministrazione per regione</h5>
-                </div>
-                <div className="w-100  h-100 d-flex justify-content-start pl-4">
-                  <p className="numeri_box">{locationTableRef}
-                  </p>
-                </div>
-                <div className="col-12 d-flex justify-content-end  pb-2">
-                  <img alt="reset-plot" src="reset_white.png" onClick={resetFilter} height={35} />
-                </div>
-              </div>
-            </div>
-
-            <div className="col-3 col-md-3 h-100 d-none d-lg-block">
-              <div style={{
-                position: 'relative',
-                // width: 300,
-                // height: 180,
-                background: '#17324D',
-                top: -90,
-                left: 40
-              }}>
-                <div className="text-white w-100">
-                  <div className="w-100  h-100 d-flex justify-content-start pt-5 pl-4">
-                    <h5>Totale punti di<br></br>somministrazione</h5>
-                  </div>
-                  <div className="w-100  h-100 d-flex justify-content-start pl-4">
-                    <p className="numeri_box">{locationTableRef}
-                    </p>
-                  </div>
-                  <div className="col-12 d-flex justify-content-end  pb-2">
-                    <img alt="reset-white" src="reset_white.png" onClick={resetFilter} height={35} />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-12 col-md-6 pt-5">
-            <div className="p-4 position-relative d-lg-none">
-              <div className="w-100 h-100 d-flex justify-content-start pr-5">
-                <img src="logo.png" width="35" height="35" alt="Logo" />
-                <h5 className="pl-3 pl-sm-1">Punti di<br /> somministrazione <br /> per regione</h5>
-              </div>
-            </div>
-            <div className="p-4 position-relative d-none d-lg-block" style={{ left: '300px', top: '190px' }}>
-              <div className="w-100 h-100 d-flex justify-content-start pr-5">
-                <img src="logo.png" width="35" height="35" alt="Logo" />
-                <h5 className="pl-3 pl-sm-1">Punti di<br /> somministrazione <br /> per regione</h5>
-              </div>
-            </div>
-            <MapAreaByDeliveryLocation
-              summary={{ ...summary }}
-              handleCountryClick={handleCountryClickLocations}
-              className="w-100 h-100"
-              setLocationRegionSelect={setLocationRegionSelect}
-              locationRegionSelect={locationRegionSelect}
-            />
-          </div>
-          <div className="col-12 col-md-6 pt-3 pl-3">
-            <LocationsTable
-              summary={{ ...summary }}
-              selected={selectedLocation}
-              className="mr-5 h-100"
-              setLocationTableRef={setLocationTableRef}
-            />
-          </div>
-
-        </div>
+        <Locations
+          data={summary}
+        />
         <div className="row">
           <div className="col-12 text-center pt-5 pb-3">
             I dati visualizzati sono disponibili all'indirizzo{" "}
