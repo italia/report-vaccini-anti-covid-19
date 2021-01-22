e03457d39170407e07709073cbc6491e46063413
k4ib4 <g.schifano@me.com>
2021-01-22 15:21:42 +0100

    Update Total.jsx

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,12 +1,13 @@
 import { React } from "react";
-/*import moment from "moment";*/
+import moment from "moment";
 
 export const Total = (props) => {
   return (
     <div className="h-100 w-100 mt-2 mb-3 max-width-total">
       <div className="timestamp mx-auto">
         <h6 className="text-center pb-4">
-          Report aggiornato al: 22-01-2021 08:03
+          Report aggiornato al: {" "}
+          {props.summary.timestamp && moment(props.summary.timestamp).format("DD-MM-YYYY HH:mm")}
         </h6>
       </div>
       {/* <div className="container"> */}
