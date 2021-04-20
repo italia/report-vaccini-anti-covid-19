0c0399e4ca8399499c6d7ffe9700b955793106e9
Daniel Arthur Gallagher <8526031+DanArthurGallagher@users.noreply.github.com>
2021-04-20 21:07:18 +0000

    refactor: update header links and images The links which work on HTTPS are directly linked with HTTPS. The images have their heights corrected to numbers instead of units. The images with fixed dimensions have both written. The alt text from the images is removed: they are decorative.

--- src/components/HeaderBar.jsx
+++ src/components/HeaderBar.jsx
@@ -13,28 +13,28 @@ export const HeaderBar = () => {
       </Header> */}
       <div className="row m-0" style={{ backgroundColor: '#0059b3' }}>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2">
-          <a href="http://www.governo.it" className="text-decoration-none" target="_blank" rel="noreferrer">
-            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <a href="https://www.governo.it" className="text-decoration-none" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="40" width="36" alt="Logo" className="logo pl-5 pr-2" />
           </a>
           <p className="pt-sm-0">
-            <a href="http://www.governo.it" target="_blank" className="text-decoration-none" rel="noreferrer">
+            <a href="https://www.governo.it" target="_blank" className="text-decoration-none" rel="noreferrer">
               Presidenza del Consiglio dei Ministri
             </a>
           </p>
         </div>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pr-sm-0 pt-2 pb-2">
-          <a className="text-decoration-none" href="http://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
-            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <a className="text-decoration-none" href="https://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="40" width="36" alt="" className="logo pl-5 pr-2" />
           </a>
           <p className="pt-sm-0">
-            <a className="text-decoration-none" href="http://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
+            <a className="text-decoration-none" href="https://www.governo.it/it/dipartimenti/commissario-straordinario-lemergenza-covid-19/15974" target="_blank" rel="noreferrer">
               Commissario Straordinario Covid-19
             </a>
           </p>
         </div>
         <div className="col-12 col-md-4 d-flex justify-content-md-center justify-content-sm-start logo-section pt-2 pb-2 pr-sm-3">
-          <a href=" http://www.salute.gov.it/portale/home.html" target="_blank" rel="noreferrer">
-            <img src="logo.svg" height="4px" alt="Logo" className="logo pl-5 pr-2" />
+          <a href="http://www.salute.gov.it/portale/home.html" target="_blank" rel="noreferrer">
+            <img src="logo.svg" height="40" alt="" className="logo pl-5 pr-2" />
           </a>
           <p className="pt-sm-0">
             <a href="http://www.salute.gov.it/portale/home.html" className="text-decoration-none" target="_blank" rel="noreferrer">
@@ -46,7 +46,7 @@ export const HeaderBar = () => {
         {/** TITLE + LOGO*/}
         <div className="col-12 d-flex flex-column justify-content-center mainBanner">
           <div className="d-flex justify-content-center">
-            <img src="logo.png" alt="Logo" className="main-logo" />
+            <img src="logo.png" height="86" width="86" alt="" className="main-logo" />
             <span className="pl-3">Report Vaccini Anti COVID-19</span>
           </div>
         </div>
